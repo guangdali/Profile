9d9f061d8f0ec38adc1e6e28197e8d6da6a57eee
guangli <guang.lsu@gmail.com>
2019-12-06 15:45:25 -0600

    Add project files.

---
+++ Profile/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Profile.Models
+{
+    public class User
+    {
+        int id { get; set; }
+        string FirstName { get; set; }
+        string MiddleName { get; set; }
+        string LastName { get; set; }
+        string DateOfBirth { get; set; }
+        string Email { get; set; }
+    }
+}
